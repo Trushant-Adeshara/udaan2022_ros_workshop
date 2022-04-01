f5d4fbcf187ba80d90b016464877038bf5da2c00
Trushant-Adeshara <trushantadeshara99@gmail.com>
2022-04-02 01:29:52 +0530

    add ros basic tutorials

---
+++ ros_basic_tutorials/scripts/chatter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Include rospy library
+import rospy
+
+# Include ros msgs
+from std_msgs.msg import String
+
+
+def main():
+	# ROS Node initializer
+	rospy.init_node('talker', anonymous=True, log_level=rospy.DEBUG)
+
+	# ROS Publisher with chatter topic
+	pub = rospy.Publisher('chatter', String, queue_size=10)
+
+	# Defining publishing rate of node
+	rate = rospy.Rate(1)
+
+	while not rospy.is_shutdown():
+		name = "Your Name %s" % rospy.get_time()
+		rospy.loginfo(name)
+		pub.publish(name)
+		rate.sleep()
+
+if __name__ == "__main__":
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
